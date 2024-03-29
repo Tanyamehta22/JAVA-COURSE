f010b14f3eda148cc14526a781dfc76b9404d3df
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2024-03-29 22:52:10 +0530

    first commit

---
+++ Hello.java
@@ -0,0 +1,20 @@
+class Hello
+{
+    public static void main(String a[])
+    {
+        int num1=9;
+        byte by = 127;
+        short sh= 558;
+        long l = 5854l;
+
+        float f= 5.8f;
+        double d = 5.8;
+
+        char c = 'k';
+        char d= '8'; //literals
+        
+        boolean b = true;
+
+        
+    }
+}
