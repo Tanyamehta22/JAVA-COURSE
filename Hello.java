2c0dbcbb9071061b5857905d9dc3e5a6216d4406
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2024-04-02 16:31:25 +0530

    sixth commit

--- Hello.java
+++ Hello.java
@@ -6,9 +6,19 @@ class Hello
         num1 += 1;
         num2 -= 2;
         num3 *= 8;
-        num1 ++; //increment same as num1= num1 +1;
+        num1 ++; //post - increment same as num1= num1 +1;
+        ++ num1; //pre - increment
         num1 --; //decrement
-        system.out.println(num1);
+
+
+
+        // DIFFERENCE BETWEEN PRE-INCREMENT AND POST-INCREMENT---
+
+        int result = ++num; // will print 8...it will first increment and then will fetch the value
+        
+        int result = num++; // will print 7...it will first fetch the value of num and then increment
+
+        system.out.println(result);
      
 
           
