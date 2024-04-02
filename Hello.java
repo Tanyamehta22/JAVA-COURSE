3363f20fab2f9ada961bdb5a34423dde421cd3f7
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2024-04-02 16:15:28 +0530

    fourth commit

--- Hello.java
+++ Hello.java
@@ -2,14 +2,23 @@ class Hello
 {
     public static void main(String a[])
     {
-        // TYPE PROMOTIONS--
+        // OPERATIONS--
+         int num1=7;
+         int num2=5;
 
-        byte a = 10;
-        byte b = 30;
+         int result1= num1 + num2
+         int result2 = num1 - num2
+         int result3 = num1 * num2
+         int result4 = num1/num2 //this will give us the quotient only
+         int result5 = num1 % num2 //this will print the remainder
 
-        int result = a*b; // as a*b ==300 which is large so it will be saved in int
-
-        system.out.println(result);
+         system.out.println(result1);
+         system.out.println(result2);
+         system.out.println(result3);
+         system.out.println(result4);
+         system.out.println(result5);
+ 
+ 
         
     }
 }
