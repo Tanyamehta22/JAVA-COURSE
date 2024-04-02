d8ef99102de15dc9c26a3f1dd4bdb42cbe4d2459
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2024-04-02 16:26:19 +0530

    fifth commit

--- Hello.java
+++ Hello.java
@@ -2,21 +2,17 @@ class Hello
 {
     public static void main(String a[])
     {
-        // OPERATIONS--
-         int num1=7;
-         int num2=5;
+        // INCREMENT--
+        num1 += 1;
+        num2 -= 2;
+        num3 *= 8;
+        num1 ++; //increment same as num1= num1 +1;
+        num1 --; //decrement
+        system.out.println(num1);
+     
 
-         int result1= num1 + num2
-         int result2 = num1 - num2
-         int result3 = num1 * num2
-         int result4 = num1/num2 //this will give us the quotient only
-         int result5 = num1 % num2 //this will print the remainder
-
-         system.out.println(result1);
-         system.out.println(result2);
-         system.out.println(result3);
-         system.out.println(result4);
-         system.out.println(result5);
+          
+          
  
  
         
