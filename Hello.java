bd29e980779f08b8c6ebc3e6bb556124778634e2
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2024-04-06 11:09:05 +0530

    if else if

--- Hello.java
+++ Hello.java
@@ -1,19 +1,18 @@
 public class Hello {
     public static void main(String[] args) {
 
-        // IF ELSE IN JAVA --
+        // IF ELSE IF IN JAVA --
         
         int  x = 5;
         int y = 7;
+        int z =9;
 
-        if (x>y)
-        {
+        if (x>y && x>z)
             System.out.println(x);
-            System.out.println("Thank you");
-        }
-    
-        else
-            System.out.println(y);    
+        else if(y>z)
+            System.out.println(y); 
+        else 
+            System.out.println(z);       
          
 
     }
