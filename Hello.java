529bd19e25d303c0a3d723ff97f0875900b97d69
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2024-04-06 11:48:12 +0530

    multidimentional array

--- Hello.java
+++ Hello.java
@@ -1,22 +1,44 @@
-// CREATION OF ARRAY IN JAVA---
+// CREATION OF MUTLIDIMENTIONAL ARRAY IN JAVA---
 
 public class Hello
 {
-    public static void main(String a[])
+    pubic static void main(String a[])
     {
-        // int nums[] = {3,7,2,4};
-        int nums[] = new int[4]; //another way to declare array
-        nums[0]=6;
-        nums[1]=3;
-        nums[2]=4;
-        nums[3]=5;
+        int nums[][] = new int[3][4];
+         
 
-        for (int i =0; i<4; i++)
+   
+        for (int i=0; i<3; i++)
         {
-            System.out.println(nums[i]);
+            for (int j=0; j<4; j++)
+            {
+                 nums[i][j] = (int)(Math.random() *10);
+                 System.outprintln(nums[i][j]);
+            }
+            
         }
-        
-       
-    }
 
+        for (int i=0; i<3; i++)
+        {
+            for (int j=0; j<4; j++)
+            {
+                System.out.print(nums[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
+        //WE CAN ALSO USE ENHANCE FOR LOOP TO PRINT ARRAY--
+
+        // for (int n[] : nums)
+        // {
+        //     for(int m:n)
+        //     {
+        //         System.out.print(m + " ");
+        //     }
+        //     System.outprintln();
+        // }
+    }
 }
+
+ 
