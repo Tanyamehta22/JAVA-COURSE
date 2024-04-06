eaabb35a3ed1aed76508ea8aa4e2f4f3e20fc6ae
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2024-04-06 11:18:27 +0530

    DO WHILE LOOP

--- Hello.java
+++ Hello.java
@@ -1,24 +1,18 @@
 public class Hello 
 {
     public static void main(String[] args) {
-    {   
-            int i = 1;
+    
 
-            while(i<=4)
-        {
-            System.out.println("hi" + i);
-            int j =1;
-            while (j<=3){
-                System.out.println("Hello" + j);
-                j++;
+        // DO WHILE LOOP-->> used to make the loop run atleast one time
+            int i = 5;
+
+            do 
+            {
+                System.out.println("Hi" + i);
+                i++;
             }
-
-            i++;
-            
-        }
-
-        System.out.println("bye" + i);
-        
+            while (i<=4);
+ 
         
 
     }
