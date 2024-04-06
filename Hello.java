6c4d7eea997227e699a0adbf49c32860c6e86628
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2024-04-06 11:11:46 +0530

    SWITCH STATEMENT

--- Hello.java
+++ Hello.java
@@ -1,37 +1,44 @@
 public class Hello {
     public static void main(String[] args) {
 
-        // TERNARY OPERATOR--
+        //SWITCH STATEMENT IN JAVA --
+
+        int n=1;
+
+        switch(n){
+            case 1:
+            System.out.println("Monday");
+            break;
+
+            case 2:
+            System.out.println("Tuesday");
+            break;
+
+            case 3:
+            System.out.println("Wednesday");
+            break;
+
+            case 4:
+            System.out.println("Thursday");
+            break;
+
+            case 5:
+            System.out.println("Friday");
+            break;
+
+            case 6:
+            System.out.println("Saturday");
+            break;
+
+            case 7: 
+            System.out.println("Sunday");
+            break;
+
+            default:
+            System.out.println("Enter a  valid number");
+
+        }
         
-         int n= 5;
-         int result =0;
-         
-        //  if (n % 2==0)
-        //     result =10;
-        //  else
-        //     result= 20;
 
-
-        // TERNARY-- CONDITION ? TRUE VALUE : FALSE VALUE;
-
-        result = n % 2==0 ? 10 : 20;
-
-        System.out.println(result);public class Hello {
-    public static void main(String[] args) {
-
-        // TERNARY OPERATOR--
-        
-         int n= 5;
-         int result =0;
-         
-        //  if (n % 2==0)
-        //     result =10;
-        //  else
-        //     result= 20;
-
-
-        // TERNARY-- CONDITION ? TRUE VALUE : FALSE VALUE;
-
-        result = n % 2==0 ? 10 : 20;
-
-        System.out.println(result);
+    }
+}
