5b4e454219d3c2f74b3c02464f21c0f162576f32
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2024-04-07 01:03:38 +0530

    STRING BUILDER

--- Hello.java
+++ Hello.java
@@ -1,16 +1,27 @@
-// MUTABLE AND IMMUTABLE IN STRINGS--
+// STRINGBUFFER AND STRINGBUILDER IN JAVA--
 public class Hello
 {
   public static void main(string a[])
   {
-    String name = "navin";
-    name = name + "reddy";
-    System.out.println("hello" + name);
+        StringBuffer sb = new StringBuffer(); // return 16 as this is the buffer size
 
-    String s1 = "Navin";
-    String s2 = "Navin";
+        StringBuffer sb = new Stringbuffer("Navin"); // will return 21 (16+5)
 
-     System.out.println(s1 == s2);
-     
+        System.out.println(sb.capacity());
+        System.out.println(sb.length()); //return 5
+
+        sb.append("Reddy")
+        System.out.println(sb) // will return Navin Reddy
+
+        String str = sb.toString(); //will convert stringbuffer to string using tostring
+
+        sb.deleteCharAt(index:2); //delete the index at the given data
+
+        sb.insert(0, "Java");
+        sb.setLength(newlength :30);
+        sb.ensureCapacity(minimumCapacity: 100);
+
+        // DIFFERENCE BETWEEN STRING BUFFER AND STRING BUILDER--
+        // STRING BUFFER IS THREAD SAFE AND STRING BUIDER IS NOT
   }
 }
