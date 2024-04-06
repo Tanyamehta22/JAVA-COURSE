ae682c0015ecce1896ba254427a9172f66d81bcc
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2024-04-06 11:07:16 +0530

    logical operator

--- Hello.java
+++ Hello.java
@@ -1,17 +1,16 @@
 public class Hello {
     public static void main(String[] args) {
 
-
-        // RELATIONAL OPERATORS-
-        
-        int x = 6;
+        // LOGICAL OPERATORS--
+        int x = 7;
         int y = 5;
+        int a = 5;
+        int b = 9;
 
-        boolean result = x <= y;
-        // boolean result = x != y;
-        // boolean result = x == y;
-    
-
+        // boolean result = x > y && a > b;
+        boolean result = x > y || a > b;
+        boolean result1 = a > b;
+        System.out.println(!result1);
         System.out.println(result);
 
     }
