eb0ee23c96ddb07ff6aa3e0ccfcca27bd3cbc90c
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2024-04-06 11:25:43 +0530

    STACK AND HEAP IN JAVA

--- Hello.java
+++ Hello.java
@@ -1,20 +1,13 @@
-// METHOD OVERLOADING--
+// STACK AND HEAP IN JAVA--
 
 class Calculator
 {
-    public int add(int n1, int n2, int n3)
-    {
-        return n1+ n2 + n3;
-    }
+    int num= 5;
 
     public int add(int n1, int n2)
     {
-        return n1+ n2;
-    }
-
-    public double add(double n1, int n2)
-    {
-        return n1+ n2;
+        System.out.println(num);
+        return n1 + n2;
     }
 }
 
@@ -22,8 +15,15 @@ public class Hello
 {
     public static void main(String a[])
     {
+        int data = 10;
+         
         Calculator obj = new Calculator();
+        Calculator obj1 = new Calculator();
         int r1 = obj.add(3,4);
-        System.out.println(r1); 
+        // System.out.println(r1); 
+
+        obj.num= 8;
+        System.out.println(obj.num);
+        System.out.println(obj1.num);
     }
 }
