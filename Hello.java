55c987af3850f8e465a0c4a161279c347179bd01
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2024-04-08 00:44:10 +0530

    DEFAULT VS PARAMETERIZED CONSTRUCTOR

--- Hello.java
+++ Hello.java
@@ -1,4 +1,4 @@
-// CONSTRUCTOR IN JAVA---
+// DEFUALT VS PARAMETERIZED CONSTRUCTOR IN JAVA---
 
 class Human 
 {
@@ -8,13 +8,22 @@ class Human
 
     //CONSRUCTOR - THEY HAVE SAME NAME AS CLASS AND ARE SIMILAR TO METHOD EXCEPT THEY DON'T HAVE A RETURN TYPE--->
     // WE DON'T NEED TO CALL A CONSTRUCTOR, WHENEVER WE CREATE AN OBJECT IT CALLS THE CONSTRUCTOR.
+    // EVEN IF WE DO NOT CREATE A CONSTRUCTOR IT WILL AUTOMATICALLY BE CREATED 
     // BY DEFAULT WE CAN  ALSO ASSIGN VALUE IN CONSTRUCTOR-->
 
+    // DEFAULT CONSTRUCTOR-->
     public Human()
     {
         // System.out.println("in constructor");
         age =12;
         name = "John";
+    }
+
+    // PARAMETERIZED CONSTRUCTOR-->
+    public Human (int a, String n)
+    {
+        age = a;
+        name = n;
     }
 
   // getter->
@@ -45,11 +54,16 @@ public class Demo
   public static void mai(String a[])
   {
     Human obj = new Human();
+    Human obj1 = new Human(a:18, n:"Navin");
+    System.out.println(obj.getName() + " : " + obj.getAge())
+    System.out.println(obj1.getName() + " : " + obj1.getAge())
+
     // obj.age=11;
     // obj.name = "Navin";
-    obj.setAge(a:30);
-    obj.setName(n: "Reddy");
 
-    System.out.println(obj.getName() + " :" + obj.getAge());
+    // obj.setAge(a:30);
+    // obj.setName(n: "Reddy");
+
+     
   }
 }
