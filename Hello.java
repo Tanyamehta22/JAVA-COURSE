75b99cee6aa5d37791432ffb226c4235b936940c
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2024-04-07 15:27:12 +0530

    ENCAPSULATION IN JAVA

--- Hello.java
+++ Hello.java
@@ -1,60 +1,41 @@
-// STATIC BLOCK--
-class Mobile 
+// ENCAPSULATION IN JAVA---
+
+class Human 
 {
+  private int age;
+  priavte String name;
 
-  // called instance variable
-  String brand;
-  int price;
-  Static String name;  // by using static we are making it common for all objects.
-
- //STATIC BLOCK--IT WILL BE CALLED ONLY ONCE->
-
-  static
+  public int getAge()
   {
-    name = "Phone";
-    System.out.println("in statis block");
-  }
-  public Mobile()
-  {
-    brand = "";
-    price = 200;
-    System.out.println("in constructor");
+    return age;
   }
 
-  public void show()
+  public void setAge(int a)
   {
-    //varible inside method is called local variable
-    // In non-static method we can use static variable
-    System.out.println(brand + " : " + price + ":" + name +":");
+      age = a;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+
+  public void setName(String n)
+  {
+    name =n;
   }
 }
 
- 
-
-public class Hello
+public class Demo
 {
-  public static void main(String a[])
+  public static void mai(String a[])
   {
-    Class.forName(className: "Mobile"); 
-    //THIS WILL CALL THE CLASS EVEN WHEN WE DON'T CREATE AN OBJECT
+    Human obj = new Human();
+    // obj.age=11;
+    // obj.name = "Navin";
+    obj.setAge(a:30);
+    obj.setName(n: "Reddy");
 
-     
-    // Mobile obj1 = new Mobile();
-    // obj1.brand = "Apple";
-    // obj1.price ="1500";
-    // // obj1.name = "SmartPhone";
-    // Mobile.name = "SmartPhone";  // static variable should be called with class name
-
-    
-   
-
-    // obj1.name = "Phone"; // we are changing the name of obj1 but it will also change the name of obj2 as well because we are using static
-
-    // obj1.show();
-    // obj2.show();
-
-    // // CALLING STATIC METHOD--
-    // Mobile.show1(obj1);
-
+    System.out.println(obj.getName() + " :" + obj.getAge());
   }
 }
