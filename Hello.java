58b325692d880ba04dac09da56df010ca874e95d
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2024-04-09 00:11:46 +0530

    INHERITANCE IN JAVA

--- Hello.java
+++ Hello.java
@@ -1,45 +1,26 @@
-//  INHERITANCE IN JAVA--
+/  INHERITANCE IN JAVA--
 
 
 public class Hello
 {
     public static void main(String a [])
     {
-        AdvCalc obj = new AdvCalc();
+        VeryAdvCalc obj = new VeryAdvCalc();
         int r1 = obj.add(7,3);
         int r2 = obj.sub(4,5);
         int r3 = obj.multi(4,5);
         int r4 = obj.div(4,5);
+        double r5 = obj.power(r3, r4);
 
-        System.out.println(r1 + "" + r2 + ""  + r3 + "" + r4);
+        System.out.println(r1 + "" + r2 + ""  + r3 + "" + r4 + "" + r5);
     }
 }
 
 
-public class AdvCalc extends Calc
+public class VeryAdvCalc extends AdvCalc
 {
-    public int multi(int n1, int n2)
-    {
-        return n1*n2;   
-    }
-
-    public int div(int n1, int n2)
-    {
-        return n1/n2;   
-    }
+ public double power(int n1, int n2)
+ {
+    return Math.pow(n1,n2);
+ }   
 }
-
-
-public class Calc
-{
-    public int add(int n1, int n2)
-    {
-        return n1 + n2;
-    }
-
-    public int sub(int n1, int n2)
-    {
-        return n1 - n2;
-    }
-}
-
