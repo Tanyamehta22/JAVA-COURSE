e0077907f0217aa4cba8d1a0d1661e7438942d8d
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2024-04-09 00:08:53 +0530

    NAMING CONVENTIONS

--- Hello.java
+++ Hello.java
@@ -1,69 +1,9 @@
-// DEFUALT VS PARAMETERIZED CONSTRUCTOR IN JAVA---
+// NAMING CONVENTIONS IN JAVA-->>
 
-class Human 
-{
-  private int age;
-  priavte String name;
+// RULES--
+// (CAMEL CASING)-->>
 
-
-    //CONSRUCTOR - THEY HAVE SAME NAME AS CLASS AND ARE SIMILAR TO METHOD EXCEPT THEY DON'T HAVE A RETURN TYPE--->
-    // WE DON'T NEED TO CALL A CONSTRUCTOR, WHENEVER WE CREATE AN OBJECT IT CALLS THE CONSTRUCTOR.
-    // EVEN IF WE DO NOT CREATE A CONSTRUCTOR IT WILL AUTOMATICALLY BE CREATED 
-    // BY DEFAULT WE CAN  ALSO ASSIGN VALUE IN CONSTRUCTOR-->
-
-    // DEFAULT CONSTRUCTOR-->
-    public Human()
-    {
-        // System.out.println("in constructor");
-        age =12;
-        name = "John";
-    }
-
-    // PARAMETERIZED CONSTRUCTOR-->
-    public Human (int a, String n)
-    {
-        age = a;
-        name = n;
-    }
-
-  // getter->
-  public int getAge()
-  {
-    return age;
-  }
-
-  //setter->
-  public void setAge(int age)
-  {
-      this.age = age; //THIS IS A KEYWORD THAT REPRESENTS CURRENT OBJECT..OBJECT WHICH IS CALLING THE METHOD.
-  }
-
-  public String getName()
-  {
-    return name;
-  }
-
-  public void setName(String name)
-  {
-    this.name =name;
-  }
-}
-
-public class Demo
-{
-  public static void mai(String a[])
-  {
-    Human obj = new Human();
-    Human obj1 = new Human(a:18, n:"Navin");
-    System.out.println(obj.getName() + " : " + obj.getAge())
-    System.out.println(obj1.getName() + " : " + obj1.getAge())
-
-    // obj.age=11;
-    // obj.name = "Navin";
-
-    // obj.setAge(a:30);
-    // obj.setName(n: "Reddy");
-
-     
-  }
-}
+// 1) CLASS AND INTERFACE -- CAPITAL LETTER EXAMPLE- Calc, Runavle
+// 2) VARIABLE AND METHOD -- SMALL LETTER EXAMPLE- marks, show()
+// 3) CONSTANTS -- PIE, BRAND
+// 4) CONSTRUCTOR-- Human()
