c7ba0bc00473f3b1b818808e914eab742014cdef
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2024-04-09 00:16:47 +0530

    DYNAMIC METHOD DISPATCH IN JAVA

--- Hello.java
+++ Hello.java
@@ -1,5 +1,48 @@
-//ACCESS MODIFIERS--->>
-// 1) PUBLIC-- CAN BE ACCESS FROM ANY class
-// 2) PRIVATE -- SAME CLASS ONLY
-// 3) DEFAULT-- CAN BE ACCESS IN SAME PACKAGE
-// 4) PROTECTED -- NOT OUTSIDE PACKAGE BUT IN SUBCLASS OF ANY PACKAGE
+// DYNAMIC METHOD DISPATCH IN JAVA--->>
+class A
+{
+    public void show()
+    {
+        System.out.println("in A Show");
+    }
+        
+    
+}
+
+class B extends A
+{
+    public void show()
+    {
+        System.out.println("in B Show");
+    }
+
+}
+
+class C extends A
+{
+    public void show()
+    {
+        System.out.println("in C Show");
+    }
+
+}
+
+ 
+
+public class Demo {
+    public static void main(String a[])
+    {
+    //    A obj = new B(); // THIS IS POSSIBLE TO CREATE REFERENCE OF A SUPERCLASS AND OBJECT OF SUBCLASS
+    //    obj.show();
+
+    // THIS IS CALLED DYNAMIC METHOD DISPATCH-- WE DO NOT KNOW WHICH MWTHOD WILL RUN..IT WILL BE DECIDED IN RUN TIME.
+       A obj = new A();  
+       obj.show();
+
+       obj = new B();
+       obj.show();
+
+       Obj = new C();
+       obj.show();
+    }
+}
