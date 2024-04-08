8e57ff077cfb3d972fb56579312320163545c4a5
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2024-04-09 00:10:53 +0530

    INHERITANCE IN JAVA'

--- Hello.java
+++ Hello.java
@@ -1,9 +1,45 @@
-// NAMING CONVENTIONS IN JAVA-->>
+//  INHERITANCE IN JAVA--
 
-// RULES--
-// (CAMEL CASING)-->>
 
-// 1) CLASS AND INTERFACE -- CAPITAL LETTER EXAMPLE- Calc, Runavle
-// 2) VARIABLE AND METHOD -- SMALL LETTER EXAMPLE- marks, show()
-// 3) CONSTANTS -- PIE, BRAND
-// 4) CONSTRUCTOR-- Human()
+public class Hello
+{
+    public static void main(String a [])
+    {
+        AdvCalc obj = new AdvCalc();
+        int r1 = obj.add(7,3);
+        int r2 = obj.sub(4,5);
+        int r3 = obj.multi(4,5);
+        int r4 = obj.div(4,5);
+
+        System.out.println(r1 + "" + r2 + ""  + r3 + "" + r4);
+    }
+}
+
+
+public class AdvCalc extends Calc
+{
+    public int multi(int n1, int n2)
+    {
+        return n1*n2;   
+    }
+
+    public int div(int n1, int n2)
+    {
+        return n1/n2;   
+    }
+}
+
+
+public class Calc
+{
+    public int add(int n1, int n2)
+    {
+        return n1 + n2;
+    }
+
+    public int sub(int n1, int n2)
+    {
+        return n1 - n2;
+    }
+}
+
