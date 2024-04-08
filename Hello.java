669ac8aefd243ea0aa64430d00da4f5b1d6e0ea2
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2024-04-09 00:14:55 +0530

    PACKAGES IN JAVA

--- Hello.java
+++ Hello.java
@@ -1,32 +1,19 @@
- // METHOD OVERRIDING IN JAVA-->>
-
-class Calc
-{
-    public int add(int n1, int n2)
-    {
-        return n1+ n2;
-    }
-
-    
-}
-
-class AdvCalc extends Calc
-{
-    // this method override the above method of A as more preferernce is given to its own method-->>
-    public int add(int n1, int n2)
-    {
-        return n1+ n2+1;
-    }
-
-
-}
+// PACKAGES IN JAVA-->>
+// package com.google.calculations; // IN THIS WAY WE CAN NAME PACKAGES
+import tool.AdvCalc;
+import tool.Calc;
+import java.util.ArrayList;
+import java.lang.*; // this is by default imported
 
 public class Hello
 {
     public static void main(String a[])
     {
-        AdvCalc obj = new AdvCalc();
-        int r1 = obj.add(n1:3, n2:4);
-        System.out.println(r1);
+        ArrayList list = new ArrayList<>();
+
+        Calc obj = new Calc();
+        AdvCalc obj1 = new AdvCalc();
+
+        System.out.println();
     }
 }
